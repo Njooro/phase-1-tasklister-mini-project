6ba9f1b1db2744002a8cbf000847a300c5bd8c54
Maxwell <njoromax5@gmail.com>
2023-07-04 16:27:57 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // document.querySelector('form').addEventListener('submit', (e) => {
+  //   e.preventDefault()
+  //   handleTodo(e.target.new_todo.value)
+  // })
 });
