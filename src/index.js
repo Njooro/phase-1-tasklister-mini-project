c02cca19ec9048316fd6ebb570de4196d7567978
Maxwell <njoromax5@gmail.com>
2023-10-23 16:26:00 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  // document.querySelector('form').addEventListener('submit', (e) => {
-  //   e.preventDefault()
-  //   handleTodo(e.target.new_todo.value)
-  // })
+  document.getElementById('create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const taskDescription = document.getElementById('new-task-description').value;
+    handleTodo(taskDescription);
+    e.target.reset();
+  });
 });
+
+function handleTodo(task) {
+  const taskList = document.getElementById('tasks');
+  const newTask = document.createElement('li');
+  newTask.innerText = task;
+  taskList.appendChild(newTask);
+}
